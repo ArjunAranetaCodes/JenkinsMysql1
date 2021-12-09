0fb0a79e8f8a25256b0da30ece81f4929b208aba
TELUS\x239686 <arjun.araneta@telus.com>
2021-12-09 19:45:53 +0800

    Test

--- testscript.sh
+++ testscript.sh
@@ -4,7 +4,7 @@ PASSWORD=$2
 echo "starting mysql database"
 mysql --user=root --password=root <<MY_QUERY
 USE db_test
-SHOW tables
+INSERT INTO tbl_test1 (fname) VALUES ('test1');
 MY_QUERY
  2>null
 echo "Backup has been done"
